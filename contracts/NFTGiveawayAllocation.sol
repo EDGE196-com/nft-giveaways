20976de6498adc323b1e80b1ba3d10c4537b81f8
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2022-01-23 18:59:32 +0530

    modified distributeNFT function

--- contracts/NFTGiveawayAllocation.sol
+++ contracts/NFTGiveawayAllocation.sol
@@ -1,3 +1,4 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract NFTGiveawayAllocation {
@@ -8,18 +9,21 @@ contract NFTGiveawayAllocation {
 
     mapping(uint => bool) checkNftInArray;
     
-
+    // this function will be modified according to actual NFT Token IDs that will be created.
+    // value of i = 1 because generally NFT token id starts from 1 and not from 0. 
     function createTokenIdArray(uint arrayLength) public {
         
-        for(uint i = 0 ; i <arrayLength ; i++){
-            totalNFT.push(i);
+        for(uint i = 1 ; i < arrayLength+1 ; i++){
+            totalNFTArray.push(i);
         }
     }
 
+    // Returns array of total NFTs 
     function getTotalNFTArray() public view returns(uint[] memory){
         return totalNFTArray;
     }
 
+    // This function will generate random number within a specified range
     function randModules(uint lowerRange,uint upperRange ) internal returns(uint){
         nonce++ ;
         uint randomNumber = uint(keccak256(abi.encodePacked(nonce, block.timestamp, block.difficulty, msg.sender)));
@@ -28,18 +32,23 @@ contract NFTGiveawayAllocation {
     // Total NFTs 1000
     // Giveaway NFTs 200 (Rare 30% = 60 , Premium 30% = 60, Normal 40% = 80)
 
- 
+    // This function has to be run three time, each for Rare, Premium and Common NFT distribution
+
     function distributeNFT(uint NumberOfNft, uint lowerRange, uint upperRange) public {
-        for (uint i =0 ; i< NumberOfNft ; i++){
+        uint i = 0;
+        while(i < NumberOfNft){
             uint randNFT = randModules(lowerRange,upperRange);
 
             if(checkNftInArray[randNFT] == false){
                 giveAwayNFTArray.push(randNFT);
                 checkNftInArray[randNFT] = true;
-            }            
+                i++ ;
+            }
         }
     }
 
+    // Returns the GiveAway NFTS randomly choosen from total NFT array
+    // First 60 IDs: rare, next 60 IDs: Premium, last 80 IDs: Common
     function getGiveAwayNFTArray() public view returns(uint[] memory){
         return giveAwayNFTArray;
     }
